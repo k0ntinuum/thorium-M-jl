a7fa5fbb16faa8548d91e0977c755b2c21f021e5
k0ntinuum <k0ntinuum@proton.me>
2023-11-05 03:45:59 -0500

    ok

--- thorium-m-jl.jl
+++ thorium-m-jl.jl
@@ -9,26 +9,16 @@ key(n) = rand(Bool,n,n)
 spin_row(k,i) = circshift(k[i,:], k[i,i]  + 1)
 spin_col(k,i) = circshift(k[:,i], k[i,i]  + 1)
 
-# spin!(k,i) =for j in 1:size(k)[begin] Bool((j+i)%2) ? k[j,:] = spin_row(k,j) : k[:,j] = spin_col(k,j) end
+
 rgb(r,g,b) =  "\e[38;2;$(r);$(g);$(b)m"
 
 red() = rgb(255,0,0);yellow() = rgb(255,255,0);white() = rgb(255,255,255);gray(h) = rgb(h,h,h)
 blue() = rgb(0,0,255);
 
-# function spin(q,p)
-#     k = copy(q)
-#     for j in 1:n[begin]
-#         if  Bool((j+p)%2) 
-#             k[j,:] = spin_row(k, j, p)
-#         else 
-#             k[:,j] = spin_col(k, j, p)
-#         end
-#     end
-#     k
-# end
 
 function encode(p,q)
     k = copy(q)
+    n = size(k)[begin]
     c = Bool[]
     for i in eachindex(p)
         push!(c,Bool((tr(k) + p[i])%2))
@@ -43,6 +33,7 @@ end
 
 function decode(c,q)
     k = copy(q)
+    n = size(k)[begin]
     p = Bool[]
     for i in eachindex(c)
         push!(p,Bool((tr(k) + c[i])%2))
@@ -57,13 +48,8 @@ end
 
 function autospin(q,c)
     k = copy(q)
+    n = size(k)[begin]
     for i in 1:n Bool(q[i,c]) ? k[i,:] = spin_row(k,i) : k[:,i] = spin_col(k,i) end
-    k
-end
-
-function self(q)
-    k = copy(q)
-    for i in 1:n k[i,:] = encrypt(q[i,:],q,n) end
     k
 end
 
@@ -76,7 +62,6 @@ function encrypt(p, q, r)
     p
 end
 
-
 function decrypt(p, q, r)
     for i in 1:r
         k = autospin(q,mod1(r + 1 - i,n))
@@ -87,6 +72,11 @@ function decrypt(p, q, r)
 end
 
 function demo()
+    n = 32
+    r = n
+    k = key(n)
+    t = 32
+    w = 16
     print(white(),"k =\n", gray(150))
     print_key(k)
     print("\n",white(),"r = \n",gray(150),r,"\n\n")
@@ -119,80 +109,5 @@ function long_demo()
     end
     print(white())
 end
-function key_eating_demo()
-    q = copy(k)
-    print(white(),"k =\n", gray(150))
-    print_key(k)
-    for i in 1:n
-    	p = k[i,:]
-        print(white(),"f( ", red(), str(p,""), white()," ) = ")
-        c  = encrypt(p,k,r)
-        q[i,:] = c
-        print(yellow(),str(c,""), "  ")
-        e = p .== c
-        print(gray(100),str(e,""), " \n")
-        d  = decrypt(c,k,r)
-        if p != d @printf "ERROR" end 	
-    end
-    print("\n")
-    print(white(),"q =\n", gray(150))
-    print_key(q)
-    for i in 1:n
-    	p = q[i,:]
-        print(white(),"f( ", red(), str(p,""), white()," ) = ")
-        c  = encrypt(p,q,r)
-        k[i,:] = c
-        print(yellow(),str(c,""), "  ")
-        e = p .== c
-        print(gray(100),str(e,""), " \n")
-        d  = decrypt(c,q,r)
-        if p != d @printf "ERROR" end 	
-    end
-    print(white())
-end
 
-function key_eating_demo_2()
-    q = copy(k)
-    for _ in 1:4
-        for i in 1:n
-            p = k[i,:]
-            print(white(),"f( ", red(), str(p," "), white()," ) = ")
-            c  = encrypt(p,k,r)
-            q[i,:] = c
-            print(yellow(),str(c," "), "  ")
-            e = p .== c
-            print(gray(100),str(e," "), " \n")
-            d  = decrypt(c,k,r)
-            if p != d @printf "ERROR" end 	
-        end
-        print("\n")
-        for i in 1:n
-            p = q[i,:]
-            print(white(),"f( ", red(), str(p," "), white()," ) = ")
-            c  = encrypt(p,q,r)
-            k[i,:] = c
-            print(yellow(),str(c," "), "  ")
-            e = p .== c
-            print(gray(100),str(e," "), " \n")
-            d  = decrypt(c,q,r)
-            if p != d @printf "ERROR" end 	
-        end
-        print("\n")
-    end
-    print(white())
-end
 
-function self_demo(k)
-    for i in 1:n
-        print_key(k)
-        print("\n")
-        k =  self(k)
-    end
-end
-
-n = 8
-r = n
-k = key(n)
-t = 128
-w = 4
-self_demo(k)
